8426ff884d0d9809e09c71e7c2f777e1e3ee935d
dev.mroz@gmail.com <dev.mroz@gmail.com>
2016-02-28 11:29:59 +0100

    scripts init

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -21,3 +21,48 @@
 //= require "components/upload.js"
 //= requure "components/tooltip.js"
 //= require "components/tabs.js"
+
+// components init
+
+if ($('.mk-lightbox').length > 0) {
+  $('.mk-lightbox').lightbox();
+}
+
+if ($('.mk-carousel').length > 0) {
+  $('.mk-carousel').carousel();
+}
+
+if ($('.mk-dropdown').length > 0) {
+  $('.mk-dropdown').dropdown();
+}
+
+if ($('.mk-tooltip').length > 0) {
+
+  // <a href="#" data-title="ToolTip Text">Tooltip Target</a>
+
+  $('.mk-tooltip').tooltip({
+    direction: "top",
+    delay: 500
+  });
+}
+
+if ($('.mk-tab').length > 0) {
+
+  $('.mk-tab').tabs();
+
+  // <nav>
+  //   <a href="#tab-one" class="tab" data-tabs-group="tab-group">One</a>
+  //   <a href="#tab-two" class="tab" data-tabs-group="tab-group">Two</a>
+  //   <a href="#tab-three" class="tab" data-tabs-group="tab-group">Three</a>
+  // </nav>
+  // <div id="tab-one">
+  //     ...
+  // </div>
+  // <div id="tab-two">
+  //     ...
+  // </div>
+  // <div id="tab-three">
+  //     ...
+  // </div>
+
+}
